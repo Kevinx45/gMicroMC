52f697ce2fd9634147cdaaa545209772451b951d
Tacocat0254 <79885027+Tacocat0254@users.noreply.github.com>
2023-07-26 17:24:37 -0500

    Add files via upload
    relocate

---
+++ inc/DNAKernelMeta.cuh
@@ -0,0 +1,14 @@
+#ifndef DNAKERNELMETA_CUH
+#define DNAKERNELMETA_CUH
+
+#include "microMC_chem.h"
+#include "DNAList.h"
+#include "global.h"
+
+
+extern __constant__  int neighborindex[27];
+extern __constant__ float min1, min2, min3, max1, max2, max3;
+
+extern  __constant__  float d_rDNA[72];
+
+#endif
