b31e8a698b84c6d4e91e7b9668bc684c32972122
Kevinx45 <68166283+Kevinx45@users.noreply.github.com>
2023-08-08 15:31:08 -0500

    Update DNAKernelMeta.cuh with chemsearch and physearch

--- inc/DNAKernelMeta.cuh
+++ inc/DNAKernelMeta.cuh
@@ -10,4 +10,40 @@ extern __constant__ float min1, min2, min3, max1, max2, max3;
 
 extern  __constant__  float d_rDNA[72];
 
+__device__ float caldistanceMeta(float3 pos1, float3 pos2);
+__device__ float3 pos2localMeta(int type, float3 pos, int index);
+__global__ void phySearchMeta(
+	int num, 
+	Edeposit* d_edrop, 
+	int* dev_chromatinIndex,
+	int* dev_chromatinStart,
+	int* dev_chromatinType, 
+	CoorBasePair* dev_straightChrom,
+	CoorBasePair* dev_segmentChrom,
+	CoorBasePair* dev_bendChrom,
+	float3* dev_straightHistone,
+	float3* dev_bendHistone, 
+	combinePhysics* d_recorde,
+	float3 *dev_chromosome, 
+	int *dev_chromosome_type,
+	int *dev_segmentIndex,
+	int *dev_segmentStart, 
+	int *dev_segmentType);
+  
+__global__ void chemSearchMeta(int num,
+  Edeposit* d_edrop, 
+	int* dev_chromatinIndex,
+	int* dev_chromatinStart,
+	int* dev_chromatinType, 
+	CoorBasePair* dev_straightChrom, 
+	CoorBasePair* dev_segmentChrom,
+	CoorBasePair* dev_bendChrom,
+	float3* dev_straightHistone,
+	float3* dev_bendHistone, 
+	combinePhysics* d_recorde,
+	float3 *dev_chromosome, 
+	int *dev_chromosome_type,
+	int *dev_segmentIndex, 
+	int *dev_segmentStart, 
+	int *dev_segmentType);
 #endif
